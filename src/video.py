9c1d32e89f2005cf95ae487f58a7d9393d293a59
saske <saskedir@mail.ru>
2023-10-25 17:27:30 +0300

    add try except

--- src/video.py
+++ src/video.py
@@ -1,6 +1,7 @@
 import os
 from googleapiclient.discovery import build
 
+
 class Video:
     """Класс для работы с видео из YouTube."""
     __YT_API_KEY = os.getenv('api_you')
@@ -13,19 +14,25 @@ class Video:
     def _init_from_api(self):
         """Получаем данные по API и инициализируем экземпляр класса ими."""
         service = self.get_service()
-        video_response = service.videos().list(
-            part='snippet,statistics',
-            id=self.__video_id
-        ).execute()
+        try:
+            video_response = service.videos().list(
+                part='snippet,statistics',
+                id=self.__video_id
+            ).execute()
 
-        video_data = video_response['items'][0]
-        snippet = video_data['snippet']
-        statistics = video_data['statistics']
+            video_data = video_response['items'][0]
+            snippet = video_data['snippet']
+            statistics = video_data['statistics']
 
-        self.title = snippet['title']
-        self.url = f'https://youtu.be/{self.__video_id}'
-        self.view_count = statistics['viewCount']
-        self.like_count = statistics['likeCount']
+            self.title = snippet['title']
+            self.url = f'https://youtu.be/{self.__video_id}'
+            self.view_count = statistics['viewCount']
+            self.like_count = statistics['likeCount']
+        except Exception as e:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     @classmethod
     def get_service(cls) -> build:
@@ -35,10 +42,3 @@ class Video:
 
     def __str__(self) -> str:
         return self.title
-
-
-
-class PLVideo(Video):
-    def __init__(self, video_id, playlist_id):
-        super().__init__(video_id)
-        self.playlist_id = playlist_id
